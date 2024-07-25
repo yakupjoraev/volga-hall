c6d8fda054552f0a72920f724e21de0352aa24c9
Yakup <yakupjoraev@gmail.com>
2024-07-25 11:02:16 +0300

    added new component also-offer

--- html/src/js/main.js
+++ html/src/js/main.js
@@ -84,3 +84,41 @@ hallTabs();
 document.querySelectorAll('.sums__item').forEach((item, index) => {
   item.style.setProperty('--index', index + 1);
 });
+
+document.querySelector('.also-offer__cards').addEventListener('wheel', function (event) {
+  if (event.deltaY !== 0) {
+    event.preventDefault();
+    this.scrollLeft += event.deltaY;
+  }
+});
+
+
+const slider = document.querySelector('.also-offer__cards');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;  // stop the fn from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
