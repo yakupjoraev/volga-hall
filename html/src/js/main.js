1449cc37c0b86be7d6a7395119de4563293ad0d2
Yakup <yakupjoraev@gmail.com>
2024-08-29 12:43:37 +0300

    added new fixes

--- html/src/js/main.js
+++ html/src/js/main.js
@@ -492,6 +492,14 @@ SmoothScroll({
 
 
 
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom = new Headroom(myElement);
+// initialise
+headroom.init();
+
+
 
 
 
